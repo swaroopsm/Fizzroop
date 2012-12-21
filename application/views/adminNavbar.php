3fe76e728d6c93348edf2f519319209cbb6d3814
Swaroop SM <swaroop.striker@gmail.com>
2012-12-22 00:29:53 +0530

    added navbar for the admin

---
+++ application/views/adminNavbar.php
@@ -0,0 +1,26 @@
+<div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner" style="min-height: 45px;">
+        <div class="container">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="/">Crackpot</a>
+          <div class="nav-collapse collapse">
+          </div><!--/.nav-collapse -->
+          <ul class="nav pull-right">
+             <li class="divider-vertical"></li>
+             <li class="dropdown">
+          	  <a href="#" class="dropdown-toggle" data-toggle="dropdown" style="font-size: 13px;"><?php echo "Display Name"; ?><b class="caret"></b></a>
+              <ul class="dropdown-menu" style="font-size: 13px;">
+              	<li><a href="/">Home</a></li>
+                <li><a href="#">Account</a></li>
+                <li class="divider"></li>
+                <li><a href="/logout">Log out!</a></li>
+              </ul>
+             </li>
+          </ul>
+        </div>
+</div>
+</div>
