534d60549b5e97f3a2f3dc9675e6beda5ec805e8
Swaroop SM <swaroop.striker@gmail.com>
2012-12-22 14:33:11 +0530

    check if method is post else show 404 page

--- application/controllers/conference.php
+++ application/controllers/conference.php
@@ -4,16 +4,22 @@
 		
 		public function create(){
 			$this->load->model("conferences");
-			$data = array(
-				"venue" => $_POST['inputVenue'],
-				"year" => $_POST['inputYear'],
-				"startDate" => $_POST['inputStartDate'],
-				"endDate" => $_POST['inputEndDate']
-			);
-			if($this->conferences->insert($data))
-				echo json_encode(array("success" => true));
-			else
-				echo json_encode(array("success" => false));
+			$method = $_SERVER['REQUEST_METHOD'];
+			if($method=="POST"){
+				$data = array(
+					"venue" => $_POST['inputVenue'],
+					"year" => $_POST['inputYear'],
+					"startDate" => $_POST['inputStartDate'],
+					"endDate" => $_POST['inputEndDate']
+				);
+				if($this->conferences->insert($data))
+					echo json_encode(array("success" => true));
+				else
+					echo json_encode(array("success" => false));
+			}
+			else{
+				show_404();
+			}
 		}
 	
 	}
