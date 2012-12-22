d5ba071002d005e7a5508f1755f0529dca22ef88
Swaroop SM <swaroop.striker@gmail.com>
2012-12-22 15:21:35 +0530

    added login form

--- application/views/login.php
+++ application/views/login.php
@@ -3,8 +3,25 @@
 	<head>
 		<title><?php echo $page_title; ?></title>
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url().'css/style.css'; ?>">
+		<style type="text/css">
+			 body {
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #f5f5f5;
+      }
+		</style>
 	</head>
 	<body>
-		<h1>Login</h1>
+		 <div class="container">
+      <?php echo form_open("session/create", array("class" => "form-signin")); ?>
+        <h2 class="form-signin-heading">Please Login</h2>
+        <input type="text" class="input-block-level" placeholder="Your Email" name="inputLoginEmail">
+        <input type="password" class="input-block-level" placeholder="Your Password" name="inputLoginPwd">
+        <label class="checkbox">
+          <input type="checkbox" value="remember-me" name="inputLoginRemember"> Remember me
+        </label>
+        <button class="btn btn-large btn-primary" type="submit">Login</button>
+      <?php echo form_close(); ?>
+    </div> 
 	</body>
 </html>
