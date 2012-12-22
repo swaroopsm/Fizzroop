405a70c5312356f8a7fef3e50f124a02d312aefa
Swaroop SM <swaroop.striker@gmail.com>
2012-12-22 10:19:01 +0530

    added validateFormEmpty that checks for emptyness in a form

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+jQuery.fn.validateFormEmpty = function(){
+	
+	var inputsTextList = $(this).find("input[type=text]");
+	var flag=0;
+	for(var i=0;i<inputsTextList.length;i++){
+		if($.trim(inputsTextList[i].value) == ''){
+			flag++;
+		}
+	}
+	if(flag>0)
+		return false;
+	else
+		return true;
+	
+};
