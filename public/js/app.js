f15662d6c78ec34761a193ed3dcb2fa8dbb1f4f2
Swaroop SM <swaroop.striker@gmail.com>
2012-12-22 12:12:46 +0530

    define asyncSubmit function

--- public/js/app.js
+++ public/js/app.js
@@ -22,3 +22,39 @@ jQuery.fn.validateFormEmpty = function(){
 		return true;
 	
 };
+
+/**
+	* Submit form via Ajax
+*/
+
+jQuery.fn.asyncSubmit = function(options){
+	
+	$(options.loadTarget).html(options.loader).show();
+	var inputsList = $(this).find("input[type=text]");
+	var paramList="";
+	for(var i=0;i<inputsList.length;i++){
+		paramList = paramList+inputsList[i].name+"= "+inputsList[i].value+"&";
+	}
+	paramList = paramList.substring(0,paramList.length-1);
+	var url = $(this).attr("action");
+	var method = $(this).attr("method");
+	$.ajax({
+		url: url,
+		type: 'POST',
+		data: paramList,
+		success: function(data){
+			var obj=jQuery.parseJSON(data);
+			var resp;
+			if(obj.success)
+				resp = "<span class='span6 alert alert-success'><a class='close' data-dismiss='alert' href='#'>&times;</a>"+options.successMsg+"</span>";
+			else
+				resp = "<span class='span6 alert alert-danger'>"+options.errorMsg+"</span>";
+			$(options.target).html(resp).hide().fadeIn(500);
+			$(options.loadTarget).html('').show();
+		},
+		error: function(data){
+			console.log(data);
+		}
+	});
+	
+}
