3120873439f1a789bd749137b079e71271619b59
Swaroop SM <swaroop.striker@gmail.com>
2012-12-22 15:03:13 +0530

    initial login view

---
+++ application/views/login.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php echo $page_title; ?></title>
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url().'css/style.css'; ?>">
+	</head>
+	<body>
+		<h1>Login</h1>
+	</body>
+</html>
