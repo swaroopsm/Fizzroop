5def9378ebf40fe781e181231d9a4800d77f1317
Swaroop SM <swaroop.striker@gmail.com>
2012-12-23 13:43:18 +0530

    set flashdata key without any value so taht it doesnt display undefined when view is loaded

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -3,7 +3,9 @@
 	class Login extends CI_Controller{
 	
 		public function index(){
+			$this->load->library("session");
 			$data['page_title'] = "Login to your account";
+			$data['message'] = $this->session->flashdata('message');
 			$this->load->view("login", $data);
 		}
 	
