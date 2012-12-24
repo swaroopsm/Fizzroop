dd07e35cd54c654f3878de10a8d5f7fb7ac63e1a
Swaroop SM <swaroop.striker@gmail.com>
2012-12-24 10:34:28 +0530

    call layouts/header file

--- application/views/adminDashboard.php
+++ application/views/adminDashboard.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<title><?php echo $page_title; ?></title>
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url().'css/style.css'; ?>">
+		<?php $this->load->view("layouts/header"); ?>
 		<style type="text/css">
 			body{
 				padding-top: 80px;
