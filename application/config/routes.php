8d46875eb417a4ef2029f1159c921d1076325bef
Swaroop SM <swaroop.striker@gmail.com>
2012-12-24 23:53:45 +0530

    created route for admin view_where function

--- application/config/routes.php
+++ application/config/routes.php
@@ -42,6 +42,7 @@ $route['default_controller'] = "welcome";
 $route['404_override'] = '';
 $route['conference/(:num)'] = "conference/view_where/$1";
 $route['attendee/(:num)'] = "attendee/view_where/$1";
+$route['admin/(:num)'] = "admin/view_where/$1";
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
