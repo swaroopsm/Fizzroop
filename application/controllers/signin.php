a847160bd7e3f2b43761b39f51b667c5f20d9b9f
Swaroop SM <swaroop.striker@gmail.com>
2012-12-24 21:21:08 +0530

    initial signin function and defined index method

---
+++ application/controllers/signin.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+	class Signin extends CI_Controller{
+	
+		public function index(){
+			$this->load->library("session");
+			$data['page_title'] = "Login to your account";
+			$data['message'] = $this->session->flashdata('message');
+			$this->load->view("signin", $data);
+		}
+	
+	}
+
+?>
