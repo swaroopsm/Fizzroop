7b95a533f1558e6180d43f37824b6ceb9b2f826a
Swaroop SM <swaroop.striker@gmail.com>
2012-12-24 21:56:37 +0530

    defined constructor function for admin controller

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -1,7 +1,14 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 	class Admin extends CI_Controller{
-	
+		
+		public function __construct(){
+			parent::__construct();
+			$this->load->library("session");
+			$this->load->library("encrypt");
+			$this->load->model("admins");
+		}
+		
 		public function index(){
 			$data['page_title'] = "Welcome Admin!";
 			$this->load->view("adminDashboard", $data);
