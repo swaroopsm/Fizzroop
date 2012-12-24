4ad0f867c4877fc8a54ea11fb46f14dcb6a51329
Swaroop SM <swaroop.striker@gmail.com>
2012-12-24 22:22:48 +0530

    defiend login function for admin controller

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -13,7 +13,31 @@
 			$data['page_title'] = "Welcome Admin!";
 			$this->load->view("adminDashboard", $data);
 		}
-	
+	 
+	 public function login(){
+	 	if($_SERVER['REQUEST_METHOD'] == "POST"){
+				$data = array(
+					"adminEmail" => $this->input->post("inputLoginEmail"),
+					"adminPassword" => $this->encrypt->sha1($this->input->post("inputLoginPwd").$this->encrypt->sha1($this->config->item("password_salt")))
+				);
+				
+				if($this->admins->view_where($data)->num_rows() > 0){
+					$this->session->set_userdata(array(
+						"loggedin" => true,
+						"email" => $this->input->post("inputLoginEmail")
+					));
+					redirect(base_url()."admin");
+				}
+				else{
+					 $this->session->set_flashdata("message", "<span class='span3 alert alert-danger'><center>Login failed!</center></span>");
+           redirect(base_url()."signin");
+				}
+			}
+			else{
+				show_404();
+			}
+	 }
+	 
 	}
 
 ?>
