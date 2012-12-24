dd40eee13f8c4e9d7d35e8fc71a6e2f17c501c67
Swaroop SM <swaroop.striker@gmail.com>
2012-12-24 16:17:59 +0530

    added comments that states the usage of the defined functions

--- application/models/attendees.php
+++ application/models/attendees.php
@@ -2,6 +2,10 @@
 
 	class Attendees extends CI_Model{
 	
+		/**
+			* Creates a new Attendee
+		*/
+		
 		public function insert($data){
 			if($this->db->insert("attendees", $data))
 				return true;
@@ -9,9 +13,19 @@
 				return false;
 		}
 		
+		
+		/**
+			* Log in an Attendee
+		*/
+		
 		public function login($data){
 			return $this->db->get_where("attendees", $data);
 		}
+		
+		
+		/**
+			* Returns all attendees
+		*/
 		
 		public function view(){
 			return $this->db->get("attendees");
