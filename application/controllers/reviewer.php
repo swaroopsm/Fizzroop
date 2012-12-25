f94f3b8e965c07c1ff617a966a3f944949343a4d
Swaroop SM <swaroop.striker@gmail.com>
2012-12-25 11:22:58 +0530

    defined create function of reviewer controller

--- application/controllers/reviewer.php
+++ application/controllers/reviewer.php
@@ -14,6 +14,37 @@
 			$this->load->library("uri");
 		}
 		
+		
+		/**
+			* Handles creation of a Reviewer.
+		**/
+		
+		public function create(){
+			if($_SERVER['REQUEST_METHOD'] == "POST"){
+				if($this->session->userdata("adminLoggedin") == true){
+					$data = array(
+						"reviewerFirstName" => $this->input->post("inputFirstName"),
+						"reviewerLastName" => $this->input->post("inputLastName"),
+						"reviewerEmail" => $this->input->post("inputEmail"),
+						"reviewerPassword" => $this->encrypt->sha1($this->input->post("inputPassword").$this->encrypt->sha1($this->config->item("password_salt")))
+					);
+					$this->reviewers->insert($data);
+					echo json_encode(
+						array(
+							"success" => true,
+							"reviewerID" => $this->db->insert_id()
+						)
+					);
+				}
+				else{
+					show_404();
+				}
+			}
+			else{
+				show_404();
+			}
+		}
+		
 	}
 
 ?>
