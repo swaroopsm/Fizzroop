74c708e7f935d58432f7811cf31a404839eb0452
Swaroop SM <swaroop.striker@gmail.com>
2012-12-25 10:44:57 +0530

    added missing paranthesis and included comments for the constructor

--- application/controllers/reviewer.php
+++ application/controllers/reviewer.php
@@ -2,14 +2,18 @@
 
 	class Reviewer extends CI_Controller{
 	
-		public function __construct{
+		/**
+			* Constructor function
+		**/
+		
+		public function __construct(){
 			parent::__construct();
 			$this->load->library("session");
 			$this->load->library("encrypt");
 			$this->load->model("reviewers");
 			$this->load->library("uri");
 		}
-	
+		
 	}
 
 ?>
