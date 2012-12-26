edc84e56d37cc4355ff8d80300903138357de0ae
Swaroop SM <swaroop.striker@gmail.com>
2012-12-26 23:11:43 +0530

    defined create function for comment controller

--- application/controllers/comment.php
+++ application/controllers/comment.php
@@ -13,6 +13,31 @@
 			$this->load->library("uri");
 		}
 	
+	 /**
+			* Handles creation of a Comment.
+		**/
+		
+		public function create(){
+			if($_SERVER['REQUEST_METHOD'] == "POST"){
+				$data = array(
+					"commentContent" => $this->input->post("inputComment"),
+					"abstractID" => $this->input->post("inputAbstractID"),
+					"reviewerID" => $this->input->post("inputReviewerID"),
+					"commentType" => $this->input->post("inputCommentType")
+				);
+				$this->comments->insert($data);
+				echo json_encode(
+					array(
+						"success" => true,
+						"commentType" => $this->db->insert_id()
+					)
+				);
+			}
+			else{
+				show_404();
+			}
+		}
+	
 	}
 
 ?>
