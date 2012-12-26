b19e9ae29fdfcb10263fb0f9bab0e77b26e2726e
Swaroop SM <swaroop.striker@gmail.com>
2012-12-26 22:54:46 +0530

    defined view_where function for comments model

--- application/models/comments.php
+++ application/models/comments.php
@@ -10,12 +10,22 @@
 			$this->db->insert("comments", $data);
 		}
 		
+		
 		/**
 			* Returns all Comments.
 		*/
 		
 		public function view(){
 			return $this->db->get("comments");
+		}
+		
+		
+		/**
+			* Returns a specific Comment subjected to a where clause. 
+		*/
+		
+		public function view_where($data){
+			return $this->db->get_where("comments", $data);
 		}
 		
 	}
