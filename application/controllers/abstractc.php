8110f46316d21469f9e5c0876227e5fae536ddf1
Swaroop SM <swaroop.striker@gmail.com>
2012-12-27 19:39:12 +0530

    initial abstract controller and defined constructor function

---
+++ application/controllers/abstractc.php
@@ -0,0 +1,19 @@
+<?php
+
+	class Abstractc extends CI_Controller{
+	
+	 /**
+	 	 * Constructor function
+	 **/
+	 
+		public function __construct(){
+			parent::__construct();
+			$this->load->library("session");
+			$this->load->model("abstracts");
+			$this->load->library("uri");
+		}
+	
+	 
+	}
+
+?>
