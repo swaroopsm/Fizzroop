f6fa6efc5c7a145e2420f58116b57165574fe509
Swaroop SM <swaroop.striker@gmail.com>
2012-12-27 12:09:43 +0530

    defined create function for score controller

--- application/controllers/score.php
+++ application/controllers/score.php
@@ -12,7 +12,39 @@
 			$this->load->model("scores");
 			$this->load->library("uri");
 		}
-	
+	 
+	 
+	 /**
+		 * Handles creation of a Score.
+		 * @TODO Need to implement Foreign Key checks.
+		**/
+		
+		public function create(){
+			if($_SERVER['REQUEST_METHOD'] == "POST"){
+				if($this->session->userdata("reviewerLoggedin") == true){
+					$data = array(
+						"abstractID" => $this->input->post("inputAbstractID"),
+						"reviewerID" => $this->input->post("inputReviewerID"),
+						"score" => $this->input->post("inputScore"),
+						"recommendation" => $this->input->post("inputRecommendation")
+					);
+					$this->scores->insert($data);
+					echo json_encode(
+						array(
+							"success" => true,
+							"scoreID" => $this->db->insert_id()
+						)
+					);
+				}
+				else{
+					show_404();
+				}
+			}
+			else{
+				show_404();
+			}
+		}
+			 
 	}
 
 ?>
