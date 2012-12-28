1b7200891b9246475399995dda018a464957bbe4
Swaroop SM <swaroop.striker@gmail.com>
2012-12-28 21:15:10 +0530

    defined create function for abstractc controller

--- application/controllers/abstractc.php
+++ application/controllers/abstractc.php
@@ -9,10 +9,38 @@
 		public function __construct(){
 			parent::__construct();
 			$this->load->library("session");
+			$this->load->library("encrypt");
 			$this->load->model("abstracts");
 			$this->load->library("uri");
 		}
-	
+		
+	 /**
+		 * Handles creation of an Abstract.
+		 * @TODO Need to implement Foreign Key checks.
+		**/
+		
+		public function create(){
+			if($_SERVER['REQUEST_METHOD'] == "POST"){
+				if($this->session->userdata("loggedin") == true){
+					$data = array(
+						"abstractTitle" => $this->input->post("inputAbstractTitle"),
+						"abstractContent" => $this->input->post("inputAbstractContent"),
+						"conferenceID" => $this->input->post("inputConferenceID"),
+						"attendeeID" => $this->input->post("inputAttendeeID")
+					);
+					$this->abstracts->insert($data);
+					echo json_encode(
+						array(
+							"success" => true,
+							"attendeeID" => $this->db->insert_id()
+						)
+					);
+				}
+			}
+			else{
+				show_404();
+			}
+		}
 	 
 	}
 
