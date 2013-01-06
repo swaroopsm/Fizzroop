8fedba9b6013708ea7a3b03c717d98cf0c1505a2
Swaroop SM <swaroop.striker@gmail.com>
2013-01-06 16:32:17 +0530

    defined variable that holds the count of abstract that have comments

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -15,17 +15,21 @@
 				$this->load->model("reviewers");
 				$this->load->model("attendees");
 				$this->load->model("scores");
+				$this->load->model("comments");
 				$a = $this->abstracts->select(array("abstractID"));
 				$approved = $this->abstracts->select_where_plain(array("abstractID"), array("approved" => 1));
 				$r = $this->reviewers->select(array("reviewerID"));
 				$at = $this->attendees->select_where_plain(array("attendeeID"), array("registered" => 1));
 				$s = $this->scores->select(array("scoreID"))->num_rows() - $this->scores->view_where(array("recommendation" => NULL))->num_rows();
+				$c = $this->comments->view_where(array());
+				$ca = ($this->comments->abs_with_comments_count());
 				$data['page_title'] = "Welcome Admin!";
 				$data['total_abstracts'] = $a->num_rows;
 				$data['approved_abstracts'] = $approved->num_rows();
 				$data['total_reviewers'] = $r->num_rows();
 				$data['registered_attendees'] = $at->num_rows();
 				$data['recommendations'] = $s;
+				$data['abstract_comments_count'] = $ca->num_rows();
 				$this->load->view("adminDashboard", $data);
 			}
 			else{
