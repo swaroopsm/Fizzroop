7e42073b3c17803eb320fce425d310a0a5bbcb31
Swaroop SM <swaroop.striker@gmail.com>
2013-01-14 03:33:21 +0530

    defined reviewer_abstract_submit function in reviewer.js

--- js/reviewer.js
+++ js/reviewer.js
@@ -75,7 +75,7 @@ $.getJSON("session", function(data){
 		$(".abstract_title").live("click", function(){
 			var abstractID = $(this).attr("href").substring(1);
 			$.getJSON("reviewer/abstract/assigned/"+abstractID, function(data){
-				console.log(data);
+				console.log(data)
 				$("#abstractModalLabel").html(data[0].abstractTitle);
 				$("#hidden_abstractID").val(data[0].abstractID);
 				$("#abstractContent").html('');
@@ -84,28 +84,28 @@ $.getJSON("session", function(data){
 					if(data[0].comments.length>0){
 						for(var i=0;i<data[0].comments.length;i++){
 							if(data[0].comments[i].commentType == 1){
-								comments = comments+"<p>Comment to Reviewer: <textarea id='comment_reviewer'>"+data[0].comments[i].commentContent+"</textarea></p>"
+								comments = comments+"<p>Comment to Reviewer: <textarea id='comment_reviewer'>"+data[0].comments[i].commentContent+"</textarea><input type='hidden' id='hidden_reviewer_commentID' value='"+data[0].comments[i].commentID+"'/></p>"
 							}
 							if(data[0].comments[i].commentType == 2){
-								comments = comments+"<p>Comment to Admin: <textarea id='comment_admin'>"+data[0].comments[i].commentContent+"</textarea></p>"
+								comments = comments+"<p>Comment to Admin: <textarea id='comment_admin' data-commentID='"+data[0].comments[i].commentID+"'>"+data[0].comments[i].commentContent+"</textarea><input type='hidden' id='hidden_admin_commentID' value='"+data[0].comments[i].commentID+"'/></p>"
 							}
 						}
 					}
 					else{
-						comments = comments+"<p>Comment to Reviewer: <textarea id='comment_reviewer'></textarea></p>"
-						comments = comments+"<p>Comment to Admin: <textarea id='comment_admin'></textarea></p>"
+						comments = comments+"<p>Comment to Reviewer: <textarea id='comment_reviewer'></textarea><input type='hidden' id='hidden_reviewer_commentID' value=''/></p>"
+						comments = comments+"<p>Comment to Admin: <textarea id='comment_admin'></textarea><input type='hidden' id='hidden_reviewer_commentID' value=''/></p>"
 					}
 				}
 				if(data[0].scores){
 					if(data[0].scores.length>0){
 						for(var j=0;j<data[0].scores.length;j++){
-							if(score==null){
-								var score_obj = $.parseJSON(data[0].scores[j].score);
-								score = score+"<p>Conservation Score: <input type='text' id='conservation_score' value='"+score_obj.conservation+"' /></p><p>Science Score: <input type='text' id='science_score' value='"+score_obj.science+"' /></p>"
+							if(data[0].scores[j].score==null){
+								var score_obj="";
+								score = score+"<input type='hidden' id='score_id' value=''/><p>Conservation Score: <input type='text' id='conservation_score' value='' /></p><p>Science Score: <input type='text' id='science_score' value='' /></p>"
 							}
 							else{
-								var score_obj="";
-								score = score+"<p>Conservation Score: <input type='text' id='conservation_score' value='' /></p><p>Science Score: <input type='text' id='science_score' value='' /></p>"
+								var score_obj = $.parseJSON(data[0].scores[j].score);
+								score = score+"<input type='hidden' id='score_id' value='"+data[0].scores[j].scoreID+"'/><p>Conservation Score: <input type='text' id='conservation_score' value='"+score_obj.conservation+"' /></p><p>Science Score: <input type='text' id='science_score' value='"+score_obj.science+"' /></p>"
 							}
 							if(data[0].scores[j].recommendation){
 								var rec = data[0].scores[j].recommendation;
@@ -119,17 +119,17 @@ $.getJSON("session", function(data){
 									case '3': recommendation = "<input type='radio' name='recommendation' id='recommendation' value='1'/>Talk <input type='radio' name='recommendation' id='recommendation' value='2'/>Poster <input type='radio' name='recommendation' id='recommendation' value='3' checked='checked'/>Reject";
 														break;
 														
-									default: recommendation = "<input type='radio' name='recommendation' id='recommendation' value='1' />Talk <input type='radio' name='recommendation' id='recommendation' value='2'/>Poster <input type='radio' name='recommendation' id='recommendation' value='3' Reject/>";
+									default: recommendation = "<input type='radio' name='recommendation' id='recommendation' value='1' />Talk <input type='radio' name='recommendation' id='recommendation' value='2'/>Poster <input type='radio' name='recommendation' id='recommendation' value='3'/>Reject";
 								}
 							}
 							else{
-								recommendation = "<input type='radio' name='recommendation' id='recommendation' value='1' />Talk <input type='radio' name='recommendation' id='recommendation' value='2'/>Poster <input type='radio' name='recommendation' id='recommendation' value='3' Reject/>";
+								recommendation = "<input type='radio' name='recommendation' id='recommendation' value='1' />Talk <input type='radio' name='recommendation' id='recommendation' value='2'/>Poster <input type='radio' name='recommendation' id='recommendation' value='3'/>Reject";
 							}
 						}
 					}
 					else{
-						score = score+"<p>Conservation Score: <input type='text' id='conservation_score' value='' /></p><p>Science Score: <input type='text' id='science_score' value='' /></p>";
-						recommendation = "<input type='radio' name='recommendation' id='recommendation' value='1' />Talk <input type='radio' name='recommendation' id='recommendation' value='2'/>Poster <input type='radio' name='recommendation' id='recommendation' value='3' Reject/>";
+						score = score+"<input type='hidden' id='score_id' value=''/><p>Conservation Score: <input type='text' id='conservation_score' value='' /></p><p>Science Score: <input type='text' id='science_score' value='' /></p>";
+						recommendation = "<input type='radio' name='recommendation' id='recommendation' value='1' />Talk <input type='radio' name='recommendation' id='recommendation' value='2'/>Poster <input type='radio' name='recommendation' id='recommendation' value='3'/>Reject";
 					}
 				}
 				else{
@@ -148,6 +148,44 @@ $.getJSON("session", function(data){
 				});
 			});
 		});
+		
+		
+		/**
+			*	reviewer_abstract_submit click function.
+		**/
+		
+	$("#reviewer_abstract_submit").live("click", function(){
+		var abstract_id = $.trim($("#hidden_abstractID").val());
+		var reviewer_id = $.trim($("#hidden_reviewerID").val());
+		var comment_reviewer = $.trim($("#comment_reviewer").val());
+		var comment_admin = $.trim($("#comment_admin").val());
+		var comment_reviewer_id = $.trim($("#hidden_reviewer_commentID").val());
+		var comment_admin_id = $.trim($("#hidden_admin_commentID").val());
+		var conservation_score = $.trim($("#conservation_score").val());
+		var science_score = $.trim($("#science_score").val());
+		var score = "{\"conservation\": "+conservation_score+", \"science\": "+science_score+"}";
+		var recommendation = $.trim($('[name=recommendation]:checked').val());
+		var scoreID = $.trim($("#score_id").val());
+		console.log(scoreID)
+		$.post(
+			"reviewer/reviewer_abstract_submit",
+			{
+				"abstractID": abstract_id,
+				"reviewerID": reviewer_id,
+				"comment_reviewer": comment_reviewer,
+				"comment_admin": comment_admin,
+				"comment_reviewer_id": comment_reviewer_id,
+				"comment_admin_id": comment_admin_id,
+				"score": score,
+				"recommendation": recommendation,
+				"scoreID": scoreID
+			},
+			function(data){
+				console.log(data);
+			}
+		);
+		return false;
+	});
 		
 	}
 });
