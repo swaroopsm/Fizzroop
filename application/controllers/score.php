6a3b12be9ad3bed5619a8519f076fae20a825a16
Swaroop SM <swaroop.striker@gmail.com>
2013-01-14 03:31:59 +0530

    modified create and update function of score controller

--- application/controllers/score.php
+++ application/controllers/score.php
@@ -19,56 +19,23 @@
 		 * @TODO Need to implement Foreign Key checks.
 		**/
 		
-		public function create(){
-			if($_SERVER['REQUEST_METHOD'] == "POST"){
+		public function create($data){
 				if($this->session->userdata("reviewerLoggedin") == true){
-					$data = array(
-						"abstractID" => $this->input->post("inputAbstractID"),
-						"reviewerID" => $this->input->post("inputReviewerID"),
-						"score" => $this->input->post("inputScore"),
-						"recommendation" => $this->input->post("inputRecommendation")
-					);
 					$this->scores->insert($data);
-					echo json_encode(
-						array(
-							"success" => true,
-							"scoreID" => $this->db->insert_id()
-						)
-					);
 				}
 				else{
 					show_404();
 				}
-			}
-			else{
-				show_404();
-			}
 		}
 		
 	 /**
 		 * Handles edit/update of a Score.
 		**/
 	
-		public function update(){
-				if($_SERVER['REQUEST_METHOD'] == "POST"){
+		public function update($data, $where){
 					if($this->session->userdata("reviewerLoggedin")){
-						$data = array(
-						"score" => $this->input->post("inputScore")
-					);
-						$where = array(
-							"abstractID" => $this->input->post("inputAbstractID"),
-							"reviewerID" => $this->input->post("inputReviewerID")
-						);
 						$this->scores->update($data, $where);
-						echo json_encode(array(
-								"success" => true
-							)
-						);
 					}
-				}
-				else{
-					show_404();
-				}
 			}	
 		
 		
