f5afc5f8973a2bff28f8fe1b903244640cdf9ed5
Swaroop SM <swaroop.striker@gmail.com>
2013-01-14 21:45:58 +0530

    call reviewersignin controller on reviewer logout

--- application/controllers/logout.php
+++ application/controllers/logout.php
@@ -6,10 +6,13 @@
 			$this->load->library("session");
 			$l = $this->session->userdata("loggedin");
 			$al = $this->session->userdata("adminLoggedin");
+			$rl = $this->session->userdata("reviewerLoggedin");
 			if($l == true)
 				$logout = "login";
-			else
+			if($al == true)
 				$logout = "signin";
+			if($rl == true)
+				$logout = "reviewersignin";
 			$this->session->unset_userdata(array(
 				"loggedin" => false,
 				"adminLoggedin" => false,
