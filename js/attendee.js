e09c7b16a215ae2a15be0a1dbeecc594334f6adc
Swaroop SM <swaroop.striker@gmail.com>
2013-01-15 20:39:46 +0530

    initial attendee.js script

---
+++ js/attendee.js
@@ -0,0 +1,70 @@
+/**
+	* Validate form for emptyness
+*/
+
+jQuery.fn.validateFormEmpty = function(){
+	
+	var inputsTextList = $(this).find("input[type=text], input[type=password], textarea, select, input[type=file]");
+	var inputsLabelList = $(this).find("label");
+	var flag = 0;
+	var errorMsg = "";
+	for(var i=0;i<inputsTextList.length;i++){
+		if($.trim(inputsTextList[i].value) == ''){
+			errorMsg = errorMsg + "<strong>" + inputsLabelList[i].outerText.substring(0, inputsLabelList[i].outerText.length-1) + "</strong>, ";
+			flag++;
+		}
+	}
+	
+	if(flag>0){
+		errorMsg = errorMsg.substring(0,errorMsg.length-2)+" cannot be empty!";
+		return {"success": false, "errorMsg": errorMsg}; 
+	}
+	else{
+		return {"success": true};
+	}
+	
+};
+
+/**
+	* Submit form via Ajax
+*/
+
+jQuery.fn.asyncSubmit = function(options){
+	
+	$(options.loadTarget).html(options.loader).show();
+	var inputsList = $(this).find("input[type=text], input[type=password]");
+	var paramList="";
+	for(var i=0;i<inputsList.length;i++){
+		paramList = paramList+inputsList[i].name+"="+inputsList[i].value+"&";
+	}
+	paramList = paramList.substring(0,paramList.length-1);
+	var url = $(this).attr("action");
+	var method = $(this).attr("method");
+	var formID = "#"+$(this).attr("id");
+	$.ajax({
+		url: url,
+		type: 'POST',
+		data: paramList,
+		success: function(data){
+			var obj=jQuery.parseJSON(data);
+			var resp;
+			if(obj.success)
+				resp = "<span class='span6 alert alert-success'>"+options.successMsg+"</span>";
+			else
+				resp = "<span class='span6 alert alert-danger'>"+options.errorMsg+"</span>";
+			$(options.target).html(resp).hide().fadeIn(500);
+			$(formID)[0].reset();
+			$(options.loadTarget).html('').show();
+		},
+		error: function(data){
+			console.log(data);
+		}
+	});
+	
+}
+
+$("form#new_abstract").submit(function(){
+	var stat = $(this).validateFormEmpty();
+	console.log(stat);
+	return false;
+});
