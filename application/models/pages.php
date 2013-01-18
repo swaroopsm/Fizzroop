8c9d88c289386d40d8c91b26cd5b14fe9a75c889
Swaroop SM <swaroop.striker@gmail.com>
2013-01-18 10:45:29 +0530

    defined view function for pages model

--- application/models/pages.php
+++ application/models/pages.php
@@ -9,7 +9,17 @@
 		public function insert($data){
 			$this->db->insert("pages", $data);
 		}
-	
+		
+		
+		/**
+			* Returns all Pages.
+		*/
+		
+		public function view(){
+			return $this->db->get("pages");
+		}
+		
+		
 	}
 
 ?>
