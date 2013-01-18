da5e49cc2913b83980d1c3afe27a17ad5c9ef6ed
Swaroop SM <swaroop.striker@gmail.com>
2013-01-18 13:45:42 +0530

    defined create function for page controller

--- application/controllers/page.php
+++ application/controllers/page.php
@@ -13,6 +13,29 @@
 			$this->load->library("uri");
 		}
 		
+		
+		/**
+			*	Handles creation of a new Page.
+		*/
+		
+		public function create(){
+			if($this->session->userdata("adminLoggedin") == true && $_SERVER['REQUEST_METHOD'] == "GET"){
+				#@TODO Think of a way to handle Page Images.
+				$data = array(
+					"pageTitle" => $this->input->post("inputPageTitle"),
+					"pageContent" => $this->input->post("inputPageContent"),
+					"pageType" => $this->input->post("inputPageType"),
+					"conferenceID" => $this->session->userdata("conferenceID")
+				);
+				$this->pages->insert($data);
+				echo json_encode(array("success" => true, "pageID" => $this->db->insert_id()));
+			}
+			else{
+				show_404();
+			}
+		}
+		
+		
 	}
 
 ?>
