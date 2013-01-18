068d552b06154726ef25e5d7f72ffe8ad33ed826
Swaroop SM <swaroop.striker@gmail.com>
2013-01-18 15:00:13 +0530

    defined view_where function for images model

--- application/models/images.php
+++ application/models/images.php
@@ -19,6 +19,16 @@
 			return $this->db->get("images");
 		}
 		
+		
+		/**
+			* Returns a specific Image(s) subjected to a where clause. 
+		*/
+		
+		public function view_where($data){
+			return $this->db->get_where("images", $data);
+		}
+		
+		
 	}
 
 ?>
