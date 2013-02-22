776f97769e4b30f3940b4cd358c50503f3b86885
Swaroop SM <swaroop.striker@gmail.com>
2013-02-22 17:48:30 +0530

    insert data into doattend if ticket number is verified

--- application/controllers/doattend.php
+++ application/controllers/doattend.php
@@ -30,23 +30,46 @@
 		**/
 		
 		public function check_ticket(){
-			$my_ticket = $this->uri->segment(3);
-			$flag = 0;
-			$url = "http://doattend.com/api/events/".$this->config->item('doAttend_event')."/participants_list.json?api_key=".$this->config->item('doAttend_key');
-			$jsonObject = json_decode(file_get_contents($url), true);
-			$participants = $jsonObject['participants'];
-			foreach($participants as $p){
-				$ticket = $p['Ticket_Number'];
-				if($ticket == $my_ticket){
-					$flag = 1;
-					break;
+			if($this->session->userdata("loggedin") == true){
+				$my_ticket = $this->uri->segment(3);
+				$flag = 0;
+				$url = "http://doattend.com/api/events/".$this->config->item('doAttend_event')."/participants_list.json?api_key=".$this->config->item('doAttend_key');
+				$jsonObject = json_decode(file_get_contents($url), true);
+				$participants = $jsonObject['participants'];
+				foreach($participants as $p){
+					$ticket = $p['Ticket_Number'];
+					if($ticket == $my_ticket){
+						$flag = 1;
+						break;
+					}
+				}
+				if($flag>0){
+					require_once(APPPATH."controllers/attendee.php");
+					$a = new Attendee();
+					$res = $a->attendee_data(array("attendeeID", "registered"), array("attendeeID" => $this->session->userdata("id")));
+					if($res->num_rows() > 0){
+						$row = $res->result();
+						if($row[0]->registered == 1){
+							echo json_encode(array("verified" => true, "responseMsg" => "User already registered"));
+						}
+						else{
+							$this->doattends->insert(
+								array(
+									"conferenceID" => $this->session->userdata('conferenceID'),
+									"doAttendUID" => 	$this->session->userdata('id'),
+									"doAttendRegID" => $my_ticket
+								)
+							);
+							echo json_encode(array("verified" => true, "responseMsg" => "User registered"));
+						}
+					}
+				}
+				else{
+					echo json_encode(array("verified" => true, "responseMsg" => "Ticket Number is invalid"));
 				}
 			}
-			if($flag>0){
-				echo json_encode(array("verified" => true));
-			}
 			else{
-				echo json_encode(array("verified" => false));
+				show_404();
 			}
 		}
 		
