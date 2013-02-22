bcc92b3cf862429b50b56e7684dd698f3e8d7625
Swaroop SM <swaroop.striker@gmail.com>
2013-02-22 17:26:10 +0530

    created create and check_ticker function in doattend controller

--- application/controllers/doattend.php
+++ application/controllers/doattend.php
@@ -14,6 +14,42 @@
 			$this->load->library("uri");
 		}
 		
+		
+		/**
+			*	Handles creation of a new doAttend entry.
+		**/
+		
+		public function create($data){
+			$this->doattends->insert($data);
+			return $this->db->insert_id();
+		}
+		
+		
+		/**
+			*	Handles checking for a doAttend ticket.
+		**/
+		
+		public function check_ticket(){
+			$my_ticket = $this->uri->segment(3);
+			$flag = 0;
+			$url = "http://doattend.com/api/events/".$this->config->item('doAttend_event')."/participants_list.json?api_key=".$this->config->item('doAttend_key');
+			$jsonObject = json_decode(file_get_contents($url), true);
+			$participants = $jsonObject['participants'];
+			foreach($participants as $p){
+				$ticket = $p['Ticket_Number'];
+				if($ticket == $my_ticket){
+					$flag = 1;
+					break;
+				}
+			}
+			if($flag>0){
+				echo json_encode(array("verified" => true));
+			}
+			else{
+				echo json_encode(array("verified" => false));
+			}
+		}
+		
 	}
 
 ?>
