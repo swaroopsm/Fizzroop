715dcc3d46b2ca21a39f8d93b667f6f3da41736f
gonecase <arjuna.shankar@gmail.com>
2013-03-27 13:15:08 +0530

    public home page files

---
+++ application/views/public/header.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+	<title>SCCS : STUDENTS' CONFERENCE ON CONSERVATION SCIENCE</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<link href='http://fonts.googleapis.com/css?family=Averia+Serif+Libre:300|Titillium+Web:300,400italic' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="<?php echo base_url(); ?>/css/home.css">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.0/jquery-ui.min.js"></script>
+</head>
+<body>
+
+<div id="header">
+	<img src="<?php echo base_url(); ?>images/logo.png" alt="">
+	<h1>STUDENTS&#8217; CONFERENCE ON<br>CONSERVATION SCIENCE</h1>
+	<h2>25 - 28 SEPTEMBER, 2013, BANGALORE</h2>
+	<p class="desc">100 Presentations | 300 of Asia's brightest conservation researchers</p>
+	<div class="shadow">
+		<img src="<?php echo base_url(); ?>images/shadow-top.png" alt="">
+	</div>
+</div>
+
+<div id="mainmenu">
+	<ul id="nav_main">
+		<li><a href="#">Plenaries</a></li>
+		<li><a href="#">Talks/Posters</a></li>
+		<li><a href="#">Workshops</a></li>
+		<li><a href="#">Special Sessions</a></li>
+	</ul>
+	<div class="timer1 yellowback"><a href="<?php echo base_url(); ?>/login">Login</a> | Register</div>
+</div>
