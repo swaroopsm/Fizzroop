95f480718e423ea644cda6c687fd5e5a7bd91c93
Swaroop SM <swaroop.striker@gmail.com>
2013-03-28 14:09:16 +0530

    created route for verification of foattend ticket number

--- application/config/routes.php
+++ application/config/routes.php
@@ -68,6 +68,7 @@ $route['attendee/abstract'] = "attendee/abstract_view";
 $route['page/(:num)'] = "page/view_where/$1";
 $route['page/view'] = "page/select_where";
 $route['image/(:num)'] = "image/view_where/$1";
+$route['doattend/verify/(:num)'] = "attendee/check_ticket/$1";
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
