3d15dcd17efbd729815d783f53ab295fa115957c
Swaroop SM <swaroop.striker@gmail.com>
2013-03-29 17:58:46 +0530

    defined generator function in csv controller

--- application/controllers/csv.php
+++ application/controllers/csv.php
@@ -10,7 +10,29 @@
 			parent::__construct();
 			$this->load->library("session");
 			$this->load->library("uri");
+			$this->load->dbutil();
+			$this->load->helper('file');
+			$this->load->helper('download');
 			$this->load->model("attendees");
+		}
+		
+		
+		/**
+			*	CSV generator function.
+		**/
+		
+		public function generator(){
+			if($this->session->userdata("adminLoggedin") == true){
+				$model = $this->uri->segment(2);
+				$q = $this->$model->export_csv(array("conferenceID" => $this->session->userdata("conferenceID")));
+				write_file($this->config->item("csv_path").$model.'.csv', $this->dbutil->csv_from_result($q));
+				$data = file_get_contents($this->config->item("csv_path").$model.'.csv'); // Read the file's contents
+				$name = "$model.csv";
+				force_download($name, $data);
+			}
+			else{
+				show_404();
+			}
 		}
 		
 		
