757cfb41c33d8c952c55d5ec0a15bdfa0bc54dfa
Swaroop SM <swaroop.striker@gmail.com>
2013-04-03 18:25:29 +0530

    return json of seats in view_where function of page controller

--- application/controllers/page.php
+++ application/controllers/page.php
@@ -84,7 +84,8 @@
 						"pageContent" => $r[0]->pageContent,
 						"conferenceID" => $r[0]->conferenceID,
 						"pageType" => $r[0]->pageType,
-						"images" => $images
+						"images" => $images,
+						"seats" => $r[0]->seats
 					);
 				}
 				echo json_encode($page_info);
