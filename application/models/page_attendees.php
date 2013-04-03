a817f8c0296ca8783790c02c616b97a04a4a9574
Swaroop SM <swaroop.striker@gmail.com>
2013-04-03 17:21:36 +0530

    defined select_where function for page_attendees model

--- application/models/page_attendees.php
+++ application/models/page_attendees.php
@@ -10,6 +10,17 @@
 			$this->db->insert("page_attendees", $data);
 		}
 		
+		
+		/**
+			* Handles select_where of an page_attendee.
+		**/
+		
+		public function select_where($data, $where){
+			$this->db->select($data);
+			$q = $this->db->get_where("page_attendees", $where);
+			return $q;
+		}
+		
 	}
 
 ?>
