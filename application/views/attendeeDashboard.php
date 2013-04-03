9ec6b8e1b974a525665f605771e224a429fee603
gonecase <arjuna.shankar@gmail.com>
2013-04-03 16:44:48 +0530

    split the submit and view abstract views

--- application/views/attendeeDashboard.php
+++ application/views/attendeeDashboard.php
@@ -13,43 +13,27 @@
 		<div class="menu-top">
 				<div class="attlinks"><img src="images/logo.png" height="60" width="60" alt=""></div>
 				<div class="attlinks">Hi <?php echo $attendeeFirstName." ".$attendeeLastName; ?>!
-				<!-- <a href="<?php echo base_url().'attendee'; ?>">Dashboard</a> |  --><a href="#" class="absubmit">Submit Abstract</a> <a href="#" class="absguide">Abstract Submission Guidelines</a> <a href="#" class="workshops">Workshops</a> <a href="#" class="burse">Bursaries</a> <a href="<?php echo base_url().'logout' ?>">Log out</a></div>
+				<?php if ($submitted == 1) { ?>
+						<a href="#" class="abview">View Abstract</a>
+				<?php } else { ?>
+						<a href="#" class="absubmit">Submit Abstract</a>
+				<?php } ?>
+
+				 
+				<a href="#" class="absguide">Abstract Submission Guidelines</a> 
+				<a href="#" class="workshops">Workshops</a> 
+				<a href="#" class="burse">Bursaries</a> 
+				<a href="<?php echo base_url().'logout' ?>">Log out</a></div>
 				<div class="clear"></div>
 		</div>
 		<div class="attcontainer">
-			<div class="message"><p>Hi <?php echo $attendeeFirstName." ".$attendeeLastName; ?>!</p>
-				<p>This page is where you can <a href="#" class="absubmit"> submit your abstract</a> and sign up for <a href="#" class="workshops">workshops</a> for SCCS. Make sure you <a href="#" class="absguide">read the abstract submission guidelines</a> before you submit. Once submitted, you cannot change your abstract, so please be sure of it.</p></div>
-			<div class="submitabstractform">
-				<h2>Submit Abstract</h2>
-				<p>Be sure you <a href="#" class="absguide">read the abstract submission guidelines</a> first!</p>
-				<?php
-					echo form_open("abstract/create", array("class" => "form form-horizontal", "id" => "new_abstract", "style" => "margin-left: 0px;"));
-					echo form_label("Abstract Title:", "inputAbstractTitle", array("class" => "control-label")); 
-					echo form_input(array("name" => "inputAbstractTitle", "id" => "inputAbstractTitle", "class" => "", "placeholder" => "Your Abstract's Title goes here"));
-				?>
-				
-				<?php
-					echo form_label("Methods:", "inputAbstractMethods", array("class" => "control-label")); 
-					echo form_textarea(array("name" => "inputAbstractMethods", "id" => "inputAbstractMethods", "class" => "", "placeholder" => "What methods did you use for your abstract?"));
-					echo form_label("Aim:", "inputAbstractAim", array("class" => "control-label")); 
-					echo form_textarea(array("name" => "inputAbstractAim", "id" => "inputAbstractAim", "class" => "", "placeholder" => "What aims have you set up?"));
-					echo form_label("Conservation:", "inputAbstractConservation", array("class" => "control-label")); 
-					echo form_textarea(array("name" => "inputAbstractConservation", "id" => "inputAbstractConservation", "class" => "", "placeholder" => "What methods did you use for your abstract?"));
-					echo form_label("Results:", "inputAbstractResults", array("class" => "control-label")); 
-					echo form_textarea(array("name" => "inputAbstractResults", "id" => "inputAbstractResults", "class" => "", "placeholder" => "What aims have you set up?"));
-				?>
-				
-				<p>Upload your graphical abstract. <em>Your image must be of this specified size and this thing.</em></p>
-				<?php
-					echo form_upload(array("name" => "inputAbstractImage"));
-				?>
-				<p>Choose your preference.</p>
-				<?php echo form_radio(array('name'=>'pref', 'id'=>'talk', 'value'=>'1', 'class'=>'radio')); ?>Talk
-				<?php echo form_radio(array('name'=>'pref', 'id'=>'poster', 'value'=>'2','class'=>'radio')); ?>Poster
-				<?php echo form_radio(array('name'=>'pref', 'id'=>'noPref', 'value'=>'3','class'=>'radio')); ?>No Preference
-				<input class="subButton" type="submit" value="Submit Abstract"/>
-				<?php echo form_close(); ?>
-			</div>
+			<?php 
+				if ($submitted == 1) {
+					$this->load->view("submitted");
+				} else {
+					$this->load->view("layouts/attendeeNavbar");
+				}
+			?>
 		</div>
 
 		<div class="guidelines">
