a0cb4591f5ff45b1fa58931376f6d38840d85e2f
Swaroop SM <swaroop.striker@gmail.com>
2013-04-03 18:30:57 +0530

    fixed seats decrement issue in create function of page_attendee controller

--- application/controllers/page_attendee.php
+++ application/controllers/page_attendee.php
@@ -26,14 +26,19 @@
 				if($pr[0]->pageType == "3"){
 					$flag = 0;
 					if($this->session->userdata("adminLoggedin") == true){
-						$this->page_attendees->insert(
-							array(
-								"pageID" => $this->input->post("inputPageID"),
-								"attendeeID" => $this->input->post("inputAttendeeID")
-							)
-						);
-						$flag = 1;
-						echo json_encode(array("success" => true));
+						$q = $this->page_attendees->select_where(array("page_attendeesID"), array("pageID" => $this->input->post("inputPageID"), "attendeeID" => $this->session->userdata("id")));
+						if($q->num_rows < 1){
+							$this->page_attendees->insert(
+								array(
+									"pageID" => $this->input->post("inputPageID"),
+									"attendeeID" => $this->input->post("inputAttendeeID")
+								)
+							);
+							$flag = 1;
+						}
+						else{
+							$flag = 0;
+						}
 					}
 					elseif($this->session->userdata("loggedin") == true){
 						$q = $this->page_attendees->select_where(array("page_attendeesID"), array("pageID" => $this->input->post("inputPageID"), "attendeeID" => $this->session->userdata("id")));
@@ -44,12 +49,11 @@
 									"attendeeID" => $this->session->userdata("id")
 								)
 							);
-							echo json_encode(array("success" => true));
+							$flag = 1;
 						}
 						else{
-							echo json_encode(array("success" => false, "message" => "Attendee has already registered for attending this workshop"));
+							$flag = 0;
 						}
-						$flag = 1;
 					}
 					else{
 						show_404();
@@ -63,6 +67,10 @@
 								"pageID" => $this->input->post("inputPageID")
 							)
 						);
+						echo json_encode(array("success" => true));
+					}
+					else{
+						echo json_encode(array("success" => false, "message" => "Attendee has already registered for attending this workshop"));
 					}
 				}
 				else{
