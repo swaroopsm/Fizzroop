3b530d043c514c289de8fe8996642ae2f98214d7
gonecase <arjuna.shankar@gmail.com>
2013-04-03 13:25:04 +0530

    made mods

--- application/views/layouts/attendeeNavbar.php
+++ application/views/layouts/attendeeNavbar.php
@@ -1,23 +1,13 @@
 <div class="navbar navbar-inverse navbar-fixed-top">
       <div class="navbar-inner" style="min-height: 45px;">
         <div class="container">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </a>
-          <a class="brand" href="/">Fizzroop</a>
-          <div class="nav-collapse collapse">
-          </div><!--/.nav-collapse -->
+          <img src="images/logo.png" alt="">
           <ul class="nav pull-right">
              <li class="divider-vertical"></li>
              <li class="dropdown">
-          	  <a href="#" class="dropdown-toggle" data-toggle="dropdown" style="font-size: 13px;"><?php echo $attendeeFirstName." ".$attendeeLastName; ?>&nbsp;<b class="caret"></b></a>
-              <ul class="dropdown-menu" style="font-size: 13px;">
-              	<li><a href="<?php echo base_url().'attendee'; ?>">Home</a></li>
-                <li><a href="#">Account</a></li>
-                <li class="divider"></li>
-                <li><a href="<?php echo base_url().'logout' ?>">Log out!</a></li>
+          	  
+              <ul class="dropdown-menu">
+              	
               </ul>
              </li>
           </ul>
