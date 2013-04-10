15defb1ad50d3c29cf58df073e13dd8ae7fd55ee
gonecase <arjuna.shankar@gmail.com>
2013-04-10 19:03:06 +0530

    fetching data dynamically

--- application/views/public/home.php
+++ application/views/public/home.php
@@ -1,108 +1,47 @@
 <div id="content">
 
-
 	<div id="gals">
 
 		<div id="gallery">
 			<div class="galwrap1">
-				<!-- <div class="slide">
-					<img src="http://lorempixel.com/g/1024/400/animals/2" alt="Placeheld">
-				</div> -->
-				<div class="slide" style="background-image: url('http://lorempixel.com/g/1024/400/animals/1')">
+				
+				<?php foreach ($plenaries as $plen) { ?>
+					
+
+				<div class="slide" style="background-image: url('<?php echo base_url(); ?>uploads/<?php echo $plen["imagepath"]; ?>')">
 					<div class="slide_overlay plen"></div>
 					<div class="slide_content">
 						<div class="plenary greenback">P L E N A R Y &rarr;</div>
-						<div class="speaker orangeback">Vidya Athreya</div>
+						<div class="speaker orangeback"><?php echo $plen['pageSubHeading']; ?></div>
 						<br>
 						<div class="plenary_title">
-							YOU AIN'T NOTHING BUT A SUGARCANE CAT: CONSERVATION OF LEOPARDS IN A HUMAN DOMINATED LANDSCAPE
+							<?php echo $plen['pageTitle']; ?>
 						</div>
 					</div>
 				</div>
-				<div class="slide" style="background-image: url('http://lorempixel.com/g/1024/400/animals/2'); background-position: center;">
-					<div class="slide_overlay plen"></div>
-					<div class="slide_content">
-						<div class="plenary greenback">P L E N A R Y &rarr;</div>
-						<div class="speaker orangeback">Speaker Name</div>
-						<br>
-						<div class="plenary_title">
-							YOU AIN'T NOTHING BUT A SUGARCANE CAT: CONSERVATION OF LEOPARDS IN A HUMAN DOMINATED LANDSCAPE
-						</div>
-					</div>
-				</div>
-				<div class="slide" style="background-image: url('http://lorempixel.com/g/1024/400/animals/3')">
-					<div class="slide_overlay plen"></div>
-					<div class="slide_content">
-						<div class="plenary greenback">P L E N A R Y &rarr;</div>
-						<div class="speaker orangeback">Mahesh Rangarajan</div>
-						<br>
-						<div class="plenary_title">
-							CONSERVATION AS IF DEMOCRACY MATTERED
-						</div>
-					</div>
-				</div>
-				<div class="slide" style="background-image: url('http://lorempixel.com/g/1024/400/animals/4')">
-					<div class="slide_overlay plen"></div>
-					<div class="slide_content">
-						<div class="plenary greenback">P L E N A R Y &rarr;</div>
-						<div class="speaker orangeback">Aaron Lobo</div>
-						<br>
-						<div class="plenary_title">
-							WHAT THE CATCH BROUGHT IN: ON BYCATCH AND OTHER INCIDENTAL MATTERS
-						</div>
-					</div>
-				</div>
+				
+				<?php } ?>
 
 				<div class="clear"></div>
 			</div>
 		</div> <!-- Gallery ends -->
 
 		<div id="wshopgall">
+			
 			<div class="galwrap2">
-				<div class="workslide" style="background-image: url('http://lorempixel.com/g/1024/400/technics/5')">
+				<?php foreach ($workshops as $plen) { ?>
+				<div class="workslide" style="background-image: url('<?php echo base_url(); ?>uploads/<?php echo $plen["imagepath"]; ?>')">
 					<div class="slide_overlay work"></div>
 					<div class="slide_content">
 						<div class="plenary yellowback">W O R K S H O P &rarr;</div>
-						<div class="speaker blueback">Speaker Name</div>
+						<div class="speaker blueback"><?php echo $plen['pageSubHeading']; ?></div>
 						<br>
 						<div class="plenary_title">
-							INTRODUCTION TO ARCGIS AND MODELLING
+							<?php echo $plen['pageTitle']; ?>
 						</div>
 					</div>
 				</div>
-				<div class="workslide" style="background-image: url('http://lorempixel.com/g/1024/400/technics/6');">
-					<div class="slide_overlay work"></div>
-					<div class="slide_content">
-						<div class="plenary yellowback">W O R K S H O P &rarr;</div>
-						<div class="speaker blueback">Speaker Name</div>
-						<br>
-						<div class="plenary_title">
-							SCIENCE AS STORY: COMMUNICATING SCIENTIFIC MATERIAL TO A GENERAL AUDIENCE
-						</div>
-					</div>
-				</div>
-				<div class="workslide" style="background-image: url('http://lorempixel.com/g/1024/400/technics/7')">
-					<div class="slide_overlay work"></div>
-					<div class="slide_content">
-						<div class="plenary yellowback">W O R K S H O P &rarr;</div>
-						<div class="speaker blueback">Speaker Name</div>
-						<br>
-						<div class="plenary_title">
-							INTRODUCTION TO GIS WITH QUANTUM GIS
-						</div>
-					</div>
-				</div>
-				<div class="workslide" style="background-image: url('http://lorempixel.com/g/1024/400/technics/8')">
-					<div class="slide_overlay work"></div>
-					<div class="slide_content">
-						<div class="plenary yellowback">W O R K S H O P &rarr;</div>
-						<div class="speaker blueback">Speaker Name</div>
-						<br>
-						<div class="plenary_title">
-							BASIC STATISTICS
-						</div>
-					</div>
-				</div>
+				<?php } ?>
 			</div>
 		</div>
 
@@ -111,7 +50,7 @@
 
 
 	<div class="wrap">
-
+		<?php // print_r($plenaries); ?>
 	</div>
 </div>
 
