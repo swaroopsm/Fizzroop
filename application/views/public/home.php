b9672a45d690971232cf5e0e68c01bd18654550a
gonecase <arjuna.shankar@gmail.com>
2013-04-11 10:02:10 +0530

    echoed timers

--- application/views/public/home.php
+++ application/views/public/home.php
@@ -1,5 +1,70 @@
 <div id="content">
 
+<?php $timerJSON = json_decode($timers,true);
+//  print_r($timerJSON);
+
+
+function daysto($when){
+	$now = time(); // or your date as well
+    $your_date = strtotime($when);
+    $datediff = $your_date - $now;
+    return floor($datediff/(60*60*24));
+}
+
+
+
+// function GetDays($sStartDate, $sEndDate){
+//   // Firstly, format the provided dates.
+//   // This function works best with YYYY-MM-DD
+//   // but other date formats will work thanks
+//   // to strtotime().
+//   $sStartDate = gmdate("Y-m-d", strtotime($sStartDate));
+//   $sEndDate = gmdate("Y-m-d", strtotime($sEndDate));
+
+//   // Start the variable off with the start date
+//   $aDays[] = $sStartDate;
+
+//   // Set a 'temp' variable, sCurrentDate, with
+//   // the start date - before beginning the loop
+//   $sCurrentDate = $sStartDate;
+
+//   // While the current date is less than the end date
+//   while($sCurrentDate < $sEndDate){
+//     // Add a day to the current date
+//     $sCurrentDate = gmdate("Y-m-d", strtotime("+1 day", strtotime($sCurrentDate)));
+
+//     // Add this new day to the aDays array
+//     $aDays[] = $sCurrentDate;
+//   }
+
+//   // Once the loop has finished, return the
+//   // array of days.
+//   return $aDays;
+// }
+
+// $sStartDate = date("Y-m-d");
+?>
+
+<div class="timer">
+	<h1><?php echo daysto($timerJSON['timerDate1']); ?></h1>
+	<h2><?php echo $timerJSON['timerTitle1']; ?></h2>
+</div>
+<div class="timer">
+	<h1><?php echo daysto($timerJSON['timerDate2']); ?></h1>
+	<h2><?php echo $timerJSON['timerTitle2']; ?></h2>
+</div>
+<div class="timer">
+	<h1><?php echo daysto($timerJSON['timerDate3']); ?></h1>
+	<h2><?php echo $timerJSON['timerTitle3']; ?></h2>
+</div>
+<div class="timer">
+	<h1><?php echo daysto($timerJSON['timerDate4']); ?></h1>
+	<h2><?php echo $timerJSON['timerTitle4']; ?></h2>
+</div>
+
+
+
+
 	<div id="gals">
 
 		<div id="gallery">
