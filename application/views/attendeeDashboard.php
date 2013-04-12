c21a2089f71cc9cc65e0cffd870bddcb502b5087
gonecase <arjuna.shankar@gmail.com>
2013-04-12 12:35:19 +0530

    made changes asked for by Hari

--- application/views/attendeeDashboard.php
+++ application/views/attendeeDashboard.php
@@ -21,7 +21,7 @@
 
 				 
 				<a href="#" class="absguide">Abstract Submission Guidelines</a> 
-				<a href="#" class="workshops">Workshops</a> 
+				<a href="#" class="workshops">Register for Workshops</a> 
 				<a href="#" class="burse">Bursaries</a> 
 				<a href="<?php echo base_url().'logout' ?>">Log out</a></div>
 				<div class="clear"></div>
@@ -39,9 +39,7 @@
 			?>
 			
 			<!-- This is the bursary section. Put this code wherever necessary. Assigned to @gonecase -->
-			<input id="bursary_for" placeholder="bursary for"/>
-			<input id="bursary_why" placeholder="bursar why">
-			<button id="submit_bursary">Submit Bursary</button>
+			
 			<!-- -->
 			
 			<?php 
@@ -79,11 +77,11 @@
 			</div>
 			<h2>Guidelines for writing each section of abstract</h2>
 			<p><strong>What conservation problem or question does your talk address?</strong></p>
-			<p>In this section you are expected to provide the general conservation context for your study, and to indicate your research question.  In the sample abstract above, the context is provided by the first sentence: “The herpetological diversity of India is poorly studied, particularly in the far north-east.” We might consider this the study’s "Big Question", and like most such questions, it is interesting but rather intractable. Your Research Question will be more specific and must be, largely, tractable. In the sample abstract, the Research Question is suggested, rather than stated directly: "What is the diversity of ground-dwelling skink species in the forests of Mouling National Park, Arunachal Pradesh?”  The Research Question is the pivotal question of any research report. If you are in doubt as to which of the questions addressed in your study is the Research Question, the clarification is simple: the Research Question is the question that the title of the paper answers (or promises to answer). The specific objectives you addressed to answer the research question should be presented in the next section. </p>
+			<p>In this section you are expected to provide the general conservation context for your study, and to indicate your research question.  In the sample abstract above, the context is provided by the first sentence: "The herpetological diversity of India is poorly studied, particularly in the far north-east." We might consider this the study’s "Big Question", and like most such questions, it is interesting but rather intractable. Your Research Question will be more specific and must be, largely, tractable. In the sample abstract, the Research Question is suggested, rather than stated directly: "What is the diversity of ground-dwelling skink species in the forests of Mouling National Park, Arunachal Pradesh?"  The Research Question is the pivotal question of any research report. If you are in doubt as to which of the questions addressed in your study is the Research Question, the clarification is simple: the Research Question is the question that the title of the paper answers (or promises to answer). The specific objectives you addressed to answer the research question should be presented in the next section. </p>
 			<p><strong>What were the main research methods you used?</strong></p>
 			<p>The Methodology describes the specific things you have done to acquire and analyse your data. In this section do not over-elaborate on the Methods that you have employed. The focus should be on the objectives – the Methods are a means to an end. Note how, in the sample abstract, the objectives still remain the main focus in this sentence: "Using quadrats and pit-fall traps, we surveyed the number of skink species in the three altitudinal ranges of the park (low: 750m-1500m; mid:1501m-2250; high: 2251m-3000m), over a two-month period (June-July) at the peak of the rainy season.". If you have multiple objectives, the methods used for each should be clearly mentioned.   .</p>
 			<p><strong>What are your most important results?</strong></p>
-			<p>The Results provide the answers to the questions represented by the Objectives. Few scientists have trouble understanding the nature of their results. However, many fill their abstracts with unimportant details, at the expense of what is much more important: discussing the most significant results. In the sample abstract, the main answers are, with respect to species caught in June-July: “low-altitude zone: 3 skink species; mid-altitude zone: 8 skink species; high-altitude zone: 5 skink species”.</p>
+			<p>The Results provide the answers to the questions represented by the Objectives. Few scientists have trouble understanding the nature of their results. However, many fill their abstracts with unimportant details, at the expense of what is much more important: discussing the most significant results. In the sample abstract, the main answers are, with respect to species caught in June-July: "low-altitude zone: 3 skink species; mid-altitude zone: 8 skink species; high-altitude zone: 5 skink species".</p>
 			<p><strong>What is the relevance of your results to conservation?</strong></p>
 			<p>The discussion component must firstly, and compulsorily, tell the readers the answer/s to the Research Question (not the objectives). In the sample abstract, the discussion starts with an extensive argument that the answers to the main Research Question can be obtained directly from the Results, i.e. the answers to the Objectives and the Research Question are, in this particular case, the same or similar. After having discussed the believability of your proposed answers to the Research Question, you should then discuss the implications of your findings for conservation. For example, in the sample abstract: "In this study, the number of species at low altitude was unexpectedly low, a likely result of increased human disturbance in this park, compared to nearby parks. We recommend a local education program to reduce the impact of shifting cultivation on skinks and other threatened species of the lower slopes of this valuable park."</p>
 			
@@ -94,13 +92,19 @@
 			<p>Graphical abstracts need to be uploaded as a single image file (formats allowed include jpeg, tiff, and pdf) in addition to the textual abstract during the submission process. The easiest way to make your graphical abstract is to compose it on a single MS Powerpoint slide (as done in the examples)  and then save that slide as an image file (formats allowed include jpeg, tiff, and pdf) to submit. Participants are however free to use any method/software to make their graphical abstract as long as the final submitted file is one of the formats prescribed.  Before submitting, make sure that the graphical abstract is of adequate resolution to be easily readable.</p>
 			<p>Please remember that the purpose of the graphical abstract is to highlight the data that you plan to present and clearly convey your main findings. Therefore graphical abstracts will only be judged according to the clarity of the message conveyed and not by their visual appeal.</p>
 			<p>Submission of both graphical and text abstracts is compulsory.  The graphical and textual abstracts, in combination, should clearly communicate the following to the reviewers i. the time period over which the study was conducted; ii. types of data that were collected and iii. the sample size (N). The reviewers will be using these as key parameters while evaluating the abstracts for selection for a talk or a poster to be presented in SCCS-Bangalore 2013."</p>
-			<p><em>The content on this page is slightly modified from prepared by Dr. Geoff Hyde, with assistance from Nandini Velho.</em></p>
+			<p><em>The content on this page is a slightly modified version of guidelines prepared by Dr. Geoff Hyde, with assistance from Nandini Velho.</em></p>
 
 		</div>
 
 		<div class="workshop">
 			<h2>Workshops</h2>
 			<p>No workshops have been finalised yet. Come back later and check it out.</p>
+		</div>
+
+		<div class="bursaries">
+			<input id="bursary_for" placeholder="bursary for"/>
+			<input id="bursary_why" placeholder="bursar why">
+			<button id="submit_bursary">Submit Bursary</button>
 		</div>
 
 
