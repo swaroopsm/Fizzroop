1430589b436763e3e7a7214c62d20d207f69d0d7
gonecase <arjuna.shankar@gmail.com>
2013-04-15 13:05:18 +0530

    modified linein accordance with haris email

--- application/views/submitted.php
+++ application/views/submitted.php
@@ -1,5 +1,5 @@
 <script>var absid = <?php  print_r($a[0]['abstractID']); ?></script>
 
 <div class="message"><p>Hi <?php echo $attendeeFirstName." ".$attendeeLastName; ?>!</p>
-<p>This page is where you can <a href="#" class="abview">view your submitted abstract</a> and sign up for <a href="#" class="workshops">workshops</a> for SCCS.</p>
+<p>You have successfully submitted your abstract to SCCS-2013. This page is where you can <a href="#" class="abview">view your submitted abstract</a> and sign up for <a href="#" class="workshops">workshops</a> for SCCS.</p>
 </div>
