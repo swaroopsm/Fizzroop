3f0a8f330c89e7d98f5072c77d7c02e5ff66bc39
Swaroop SM <swaroop.striker@gmail.com>
2013-04-20 16:07:24 +0530

    defeined base_url variable

--- application/views/reviewerDashboard.php
+++ application/views/reviewerDashboard.php
@@ -81,6 +81,8 @@
 	<script src="<?php echo base_url().'js/vendor.js'; ?>"></script>
 	<script src="<?php echo base_url().'js/jquery.dataTables.min.js'; ?>"></script>
 	<script>
+		base_url = "<?php echo base_url(); ?>";
+		
 		jQuery.ajaxSetup({
 			data: {
 				 <?php echo $this->security->get_csrf_token_name(); ?>: '<?php echo $this->security->get_csrf_hash(); ?>'
